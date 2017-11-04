5dd7bf65bd38ddc0f244bbee2d38a202469339dc
John Hohman <jwhohman@gmail.com>
2017-11-04 12:36:30 -0700

    redo

--- run_analysis.r
+++ run_analysis.r
@@ -22,26 +22,33 @@ activitylabels<- read.table("activity_labels.txt")
 #combine subject
 sub_data <- rbind(subject_test,subject_train)
                 
-#combiney data
+#combine y data
 y_data <- rbind(y_test,y_train)
 
-#combiney data
+#combine x data
 x_data <- rbind(x_test,x_train)
 
+#begin code to extract only mean & std dev cols from x data
+
+#extract mean and std dev col #s from features
 f1 <- grep("-(mean|std)\\(\\)", features[, 2])
+
+#extract mean/std cols from x_data
 x_data <- x_data[, f1]
-names(x_data) <- features[f1,2]
+#Colnames of x_data lbeled appropriatey
+names(x_data) <- features[f1,2] #10299 x 66
 
 #combine all
-all_data <- cbind(sub_data,y_data,x_data)
+all_data <- cbind(sub_data,y_data,x_data) #10299 x 68
 #rename cols 1 &2
 colnames(all_data)[1:2] <- c("subject","activity")
 
-#mean tidy table
-x1 <- all_data[,-1:-2]
-x2 <- all_data[,1]
-x3 <- all_data[,2]
-avg_dat <- aggregate(x=x1,by=list(x2,x3),FUN="mean")
+#change acivity values to descriptive names
+all_data[,2] <- activitylabels[all_data[,2],2]
+
+
+#generate mean tidy table
+avg_dat <- ddply(all_data, .(subject,activity),function(all_data) colMeans(all_data[,3:68]))
 write.table(avg_dat, "averages_data.txt", row.name=FALSE)
 
 
